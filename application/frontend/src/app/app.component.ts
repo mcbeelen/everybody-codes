0d7f8d029d2518b94c68511c7e4dba81aebddbfe
Marco Beelen <mbeelen@davincigroep.nl>
2019-12-23 14:00:40 +0100

    Started to replace generated app with skeleton for CameraListApplication

--- application/frontend/src/app/app.component.ts
+++ application/frontend/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'camera-application',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Camera Frontend Application';
 }
--- application/frontend/src/app/app.module.ts
+++ application/frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CameraListComponent } from './camera-list/camera-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CameraListComponent
   ],
   imports: [
     BrowserModule,
